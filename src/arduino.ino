3d51eed311c76bd30c8b690bf46a2377a649852d
conner120 <conner@reitmeier.org>
2017-01-02 20:53:36 -0600

    sensor reader
    added sensor reading and output to serial started to work on the code
    to find the next path if  forward is blocked preferring to go right

--- src/arduino.ino
+++ src/arduino.ino
@@ -1,120 +1,87 @@
-/*********************************************************************
-  This is an example for our nRF51822 based Bluefruit LE modules
-
-  Pick one up today in the adafruit shop!
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  MIT license, check LICENSE for more information
-  All text above, and the splash screen below must be included in
-  any redistribution
-*********************************************************************/
 
 #include <Arduino.h>
 #include <SPI.h>
 #if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
 #include <SoftwareSerial.h>
 #endif
-//#include <colisionFuncs.ino>
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-
+int rearFallSensorPin  = 49; //rear fall sensor pin number
+boolean rearFallSensorState = true;
+int frontFallSensorPin = 46;
+boolean frontFallSensorState = true;
+int frontLeftSensorPin = 44;
+boolean frontLeftSensorState = true;
+int sideRightSensorPin = 45;
+boolean sideRightSensorState = true;
+int frontRightSensorPin = 43;
+boolean frontRightSensorState = true;
+int sideLeftSensorPin = 41;
+boolean sideLeftSensorState = true;
+int frontCenterSensorPin = 42;
+boolean frontCenterSensorState = true;
+int rearLeftSensorPin = 40;
+boolean rearLeftSensorState = true;
+int rearCenterSensorPin = 38;
+boolean rearCenterSensorState = true;
+int rearRightSensorPin = 39;
+boolean rearRightSensorState = true;
 #include "BluefruitConfig.h"
-//String commands[2] ={"ebraek","test"}
-/*=========================================================================
-    APPLICATION SETTINGS
+//diffine sector avalibility
+boolean forward = true;
+boolean forwardRight = true;
 
-      FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-     
-                                Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                                running this at least once is a good idea.
-     
-                                When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                                Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-         
-                                Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    MODE_LED_BEHAVIOUR        LED activity, valid options are
-                              "DISABLE" or "MODE" or "BLEUART" or
-                              "HWUART"  or "SPI"  or "MANUAL"
-    -----------------------------------------------------------------------*/
-#define FACTORYRESET_ENABLE         1
+
+
+#define FACTORYRESET_ENABLE         0
 //#define BUFSIZE                        48   // Size of the read buffer for incoming data
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 #define MODE_LED_BEHAVIOUR          "MODE"
-/*=========================================================================*/
 
-// Create the bluefruit object, either software serial...uncomment these lines
+SoftwareSerial bluefruitSS = SoftwareSerial(51, 50);
 
-SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
-
-Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
-                              BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+Adafruit_BluefruitLE_UART ble(bluefruitSS, 53,
+                              52, 48);
 
 
-/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
-//Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
-
-/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
-//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
-//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
-//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
-//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-
+int mode = 3;
+String str = "";
 // A small helper
 void error(const __FlashStringHelper*err) {
+if (mode>0){
   Serial.println(err);
+}
   while (1);
 }
 
-/**************************************************************************/
-/*!
-    @brief  Sets up the HW an the BLE module (this function is called
-            automatically on startup)
-*/
-/**************************************************************************/
 void setup(void)
 {
+  pinMode(rearFallSensorPin, INPUT);
   while (!Serial);  // required for Flora & Micro
   delay(500);
 
   Serial.begin(115200);
+if (mode>0){
   Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
   Serial.println(F("------------------------------------------------"));
 
   /* Initialise the module */
   Serial.print(F("Initialising the Bluefruit LE module: "));
-
+}
   if ( !ble.begin(VERBOSE_MODE) )
   {
     error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
   }
+  if (mode>0){
   Serial.println( F("OK!") );
-
+}
   if ( FACTORYRESET_ENABLE )
   {
     /* Perform a factory reset to make sure everything is in a known state */
+    if (mode>0){
     Serial.println(F("Performing a factory reset: "));
+    }
     if ( ! ble.factoryReset() ) {
       error(F("Couldn't factory reset"));
     }
@@ -122,29 +89,33 @@ void setup(void)
 
   /* Disable command echo from Bluefruit */
   ble.echo(false);
-
+if (mode>0){
   Serial.println("Requesting Bluefruit info:");
+}
   /* Print Bluefruit information */
   ble.info();
-
+if (mode>0){
   Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
   Serial.println(F("Then Enter characters to send to Bluefruit"));
   Serial.println();
-
+}
   ble.verbose(false);  // debug info is a little annoying after this point!
 
-  /* Wait for connection */
+  /* Wait for connection
   while (! ble.isConnected()) {
     delay(500);
   }
-
+*/
+if (mode>0){
   Serial.println(F("******************************"));
-
+}
   // LED Activity command is only supported from 0.6.6
   if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
   {
     // Change Mode LED Activity
+    if (mode>0){
     Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    }
     ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
     //ble.sendCommandCheckOK("AT+BLEPOWERLEVEL=4");
   }
@@ -152,8 +123,9 @@ void setup(void)
   // Set module to DATA mode
   Serial.println( F("Switching to DATA mode!") );
   ble.setMode(BLUEFRUIT_MODE_DATA);
-
+if (mode>0){
   Serial.println(F("******************************"));
+}
 }
 
 /**************************************************************************/
@@ -163,40 +135,190 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
-  // Check for user input
-  char n, inputs[BUFSIZE + 1];
-
-  if (Serial.available())
-  {
-    n = Serial.readBytes(inputs, BUFSIZE);
-    inputs[n] = 0;
-    // Send characters to Bluefruit
-    Serial.print("Sending: ");
-    Serial.println(inputs);
-
-    // Send input data to host via Bluefruit
-    ble.print(inputs);
-  }
+  //delay(2500);
+  //Serial.println("Entering Check Sensors Function");
+  checkSensors();
   if (ble.available()) {
-    String str = "";
-    // Echo received data
+
+    // Echo receive data
     while ( ble.available() )
     {
       byte c = ble.read();
-      str =+ char(c);
-      Serial.print(c);
-      Serial.print(" or ");
-      Serial.println((char)c);
-
+      str = str + char(c);
       // Hex output too, helps w/debugging!
       //    Serial.print(" [0x");
       //    if (c <= 0xF) Serial.print(F("0"));
       //    Serial.print(c, HEX);
       //    Serial.print("] ");
       if (c == '/') {
-        Serial.println();
+        if (mode>1){
+          Serial.println(str);
+          Serial.println(F("Running Command Checker..."));
+        }
+        if (str[0]=='r'){
+          float motorValue = str.substring(1,str.length()-1).toFloat()/5;
+          Serial.println(str.substring(1,str.length()-1));
+          Serial.println("Right Motor run at: "+String(motorValue)+"%");
+        }else if(str[0]=='l'){
+          float motorValue = str.substring(1,str.length()-1).toFloat()/5;
+          Serial.println(str.substring(1,str.length()-1));
+          Serial.println("Right Motor run at: "+String(motorValue)+"%");
+        }else{
+          checkCommand(str);
+        }
+        str="";
         break;
-      }
     }
   }
+}
+delay(100);
+}
+void checkSensors(){
+  //Serial.println("Entering Check Sensors Function");
+  Serial.print("s");
+  if (irSensorCheck(sideLeftSensorPin)){
+    Serial.print("1");
+    sideLeftSensorState = false;
+    stop();
+  }else{
+    Serial.print("0");
+    sideLeftSensorState = true;
+  }
+  if (irSensorCheck(frontRightSensorPin)){
+    Serial.print("1");
+    frontRightSensorState = false;
+    stop();
+  }else{
+    frontRightSensorState = true;
+    Serial.print("0");
+  }
+  if (irSensorCheck(rearLeftSensorPin)){//4
+      Serial.print("1");
+      rearLeftSensorState = false;
+      stop();
+    }else{
+      rearLeftSensorState = true;
+      Serial.print("0");
+    }
+
+  if (irSensorCheck(sideRightSensorPin)){//1
+    Serial.print("1");
+    sideRightSensorState = false;
+    stop();
+  }else{
+    Serial.print("0");
+    sideRightSensorState = true;
+  }
+    if (irSensorCheck(frontLeftSensorPin)){//5
+      Serial.print("1");
+      frontLeftSensorState = false;
+      stop();
+    }else{
+      Serial.print("0");
+      frontLeftSensorState = true;
+    }
+    if (irSensorCheck(rearRightSensorPin)){//2
+      Serial.print("1");
+      rearRightSensorState = false;
+      stop();
+    }else{
+      Serial.print("0");
+      rearRightSensorState = true;
+    }
+        if (irSensorCheck(frontCenterSensorPin)){
+          Serial.print("1");
+          frontCenterSensorState = false;
+          stop();
+        }else{
+          Serial.print("0");
+          frontCenterSensorState = true;
+        }
+  if (irSensorCheck(rearCenterSensorPin)){
+    Serial.print("1");
+    rearCenterSensorState = false;
+    stop();
+  }else{
+    Serial.print("0");
+    rearCenterSensorState = true;
+  }
+
+
+
+  if (irSensorCheck(rearFallSensorPin)){//3
+    Serial.print("1");
+    rearFallSensorState = false;
+    stop();
+  }else{
+    Serial.print("0");
+    rearFallSensorState = true;
+  }
+
+
+
+    if (irSensorCheck(frontFallSensorPin)){//4
+        Serial.print("1");
+        frontFallSensorState = false;
+        stop();
+      }else{
+        Serial.print("0");
+        frontFallSensorState = true;
+      }
+
+
+
+  //
+
+  // if (irSensorCheck(frontFallSensorPin)){
+  //   Serial.print("1");
+  //   stop();
+  // }else{
+  //   Serial.print("0");
+  // }
+    Serial.println();
+  //delay(2000);
+}
+void checkCommand(String str){
+
+}
+boolean irSensorCheck(int pin){
+  //Serial.println(digitalRead(pin));
+  int sensorResult = digitalRead(pin);
+  //Serial.print("pin: "+String(pin)+" reads: ");
+  //Serial.println(sensorResult);
+  if (sensorResult==1){
+    if (mode>0){
+    Serial.println("NO OBJECT PRESENT on pin: "+String(pin));
+    }
+    return true;
+  }else{
+    if(mode>0){
+    Serial.println("OBJECT PRESENT pin: "+String(pin));
+    }
+    return false;
+  }
+  //delay(2500);
+}
+
+
+// motor controller Functions
+
+void stop() {
+
+}
+void findAvaliblePath(){
+    if (frontCenterSensorState){
+      return 1;
+    }
+    if (frontRightSensorState) {
+      if (sideRightSensorState){
+        return 3;
+      }
+      return 2;
+    }
+    if (frontLeftSensorState){
+      if (sideLeftSensorState){
+        return 5;
+      }
+      return 4;
+    }
 }
